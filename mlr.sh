cc36c114421f23f54df53864e51de73d6ebbadbe
rahulchittimalla <rahulchittimalla@gmail.com>
2018-10-30 00:05:44 +0530

    “report”

---
+++ mlr.sh
@@ -0,0 +1,2 @@
+
+python mlr.py --epochs=100 --lr=0.1 --batch_size=300 --train_file=/scratchd/home/rahul/DBPedia.full/full_train.txt --dev_file=/scratchd/home/rahul/DBPedia.full/full_devel.txt --test_file=/scratchd/home/rahul/DBPedia.full/full_test.txt --save_path=/home/rahul/mlld/assgn2/models
---
+++ vmlr.sh
@@ -0,0 +1,2 @@
+
+python vmlr.py --epochs=100 --lr=5 --decay=0.95 --batch_size=300 --train_file=/scratchd/home/rahul/DBPedia.full/full_train.txt --dev_file=/scratchd/home/rahul/DBPedia.full/full_devel.txt --test_file=/scratchd/home/rahul/DBPedia.full/full_test.txt --save_path=/home/rahul/mlld/assgn2/models
